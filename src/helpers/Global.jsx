66b18cd20db4698c87c0ddfafffd05cc6fa167e4
LuisEduardoRodriguez <97996472+LuisEduardoRodriguez@users.noreply.github.com>
2024-09-05 07:23:44 -0500

    Error404

---
+++ src/helpers/Global.jsx
@@ -0,0 +1,10 @@
+//Ruta del Backend  desplegado
+export const Global = {
+    url: "https://backend-snet-p3cb.onrender.com/api/"
+}
+
+
+//Ruta del Backend en local
+//export const Global = {
+//    url: "https://localhost:3900/api/"
+//}
